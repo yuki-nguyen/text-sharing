d1b65057c4f6b63622c661aa8bc6cc3c57f233f4
yuki-nguyen <tuyet.nguyenthi3009@gmail.com>
2018-07-24 17:26:04 +0700

    Text Sharing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
